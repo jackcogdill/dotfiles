c236e9396d52ded22a587d46b89c89f7014b8f91
Jack Cogdill <jackcogdill@users.noreply.github.com>
2018-11-16 14:12:07 -0500

    install.sh: only run mac confic on OSX

--- install.sh
+++ install.sh
@@ -41,6 +41,8 @@ ln "$ln_params" "$dotfiles/vim/init.vim" "$HOME/.vimrc" # Vim
 
 # macOS config
 # ================
-# Key repeat
-defaults write -g InitialKeyRepeat -int 15 # normal minimum is 15 (225 ms)
-defaults write -g KeyRepeat -int 1 # normal minimum is 2 (30 ms)
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    # Key repeat
+    defaults write -g InitialKeyRepeat -int 15 # normal minimum is 15 (225 ms)
+    defaults write -g KeyRepeat -int 1 # normal minimum is 2 (30 ms)
+fi
