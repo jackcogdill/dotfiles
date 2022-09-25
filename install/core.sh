05eaa9c56167f659274f1761fe42dbb536bb20b7
Jack Cogdill <hi@jackcogdill.com>
2022-09-25 14:39:13 -0700

    install: get nerd font scripts

--- install/core.sh
+++ install/core.sh
@@ -34,5 +34,17 @@ stow --target ~/.config/nvim nvim
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 # Fonts
+( # Install nerd font scripts
+  cd /tmp
+  git clone https://github.com/ryanoasis/nerd-fonts \
+    --depth 1 \
+    --filter=blob:none \
+    --sparse
+  cd nerd-fonts
+  git sparse-checkout set bin
+  mkdir -p ~/.local/share/fonts
+  mv bin/scripts/lib/*.sh ~/.local/share/fonts
+  rm -rf /tmp/nerd-fonts
+)
 echo "Download the font 'BlexMono Nerd Font Mono' from https://github.com/ryanoasis/nerd-fonts/releases/latest"
 echo "Download the font 'IBM Plex Mono' from https://github.com/IBM/plex/releases/latest"
