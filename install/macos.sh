f15808dc0802ef34ae92925e932b48c974f358f1
Jack Cogdill <hi@jackcogdill.com>
2022-10-06 21:57:52 -0700

    install: require installing core explicitly, not called from macos

--- install/macos.sh
+++ install/macos.sh
@@ -1,7 +1,3 @@
-cd $(dirname "$0")
-
-source core.sh
-
 # Key repeat
 defaults write -g InitialKeyRepeat -int 15 # normal minimum is 15 (225 ms)
 defaults write -g KeyRepeat -int 1 # normal minimum is 2 (30 ms)
