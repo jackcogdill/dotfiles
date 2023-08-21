18e75135388c1a2bc5184e5b6d9d7fcdc8af859f
Jack Cogdill <hi@jackcogdill.com>
2023-08-21 15:31:14 -0700

    vim: add back useful workspace plugin

---
+++ nvim/lua/plugins/vim-workspace.lua
@@ -0,0 +1,18 @@
+return {
+  'thaerkh/vim-workspace',
+  lazy = false,
+  keys = {
+    {
+      '<Leader>w',
+      function()
+        vim.cmd('ToggleWorkspace')
+      end,
+    },
+  },
+  config = function()
+    vim.g.workspace_autocreate = 1
+    vim.g.workspace_session_directory = vim.fn.stdpath('data') .. '/sessions'
+    vim.g.workspace_autosave = 0
+    vim.g.workspace_session_disable_on_args = 1 -- mostly because `hg commit` creates a workspace flooded with tmp files
+  end,
+}
