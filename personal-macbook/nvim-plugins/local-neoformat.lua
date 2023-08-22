57a547a1923145822e6aeae06a9f76f7255372a1
Jack Cogdill <hi@jackcogdill.com>
2023-08-22 16:57:02 -0700

    vim: convert neoformat vimscript to lua and change load event

--- personal-macbook/nvim-plugins/local-neoformat.lua
+++ personal-macbook/nvim-plugins/local-neoformat.lua
@@ -1,18 +1,16 @@
 -- formatting
 return {
   'sbdchd/neoformat',
-  event = 'BufWritePre',
+  event = 'VeryLazy',
   config = function()
-    vim.cmd([[
-      let g:neoformat_python_yapf = {
-            \ 'exe': 'yapf',
-            \ 'args': ["--style='{based_on_style: google, indent_width: 2}'"],
-            \ }
-      let g:neoformat_typescript_prettier = {
-            \ 'exe': 'prettier',
-            \ 'args': ['--parser typescript --single-quote'],
-            \ }
-    ]])
+    vim.g.neoformat_python_yapf = {
+      exe = 'yapf',
+      args = { "--style='{based_on_style: google, indent_width: 2}'" }
+    }
+    vim.g.neoformat_typescript_prettier = {
+      exe = 'prettier',
+      args = { '--parser typescript --single-quote' }
+    }
     vim.g.neoformat_enabled_lua = { 'stylua' }
     vim.api.nvim_create_autocmd('BufWritePre', {
       pattern = '*',
