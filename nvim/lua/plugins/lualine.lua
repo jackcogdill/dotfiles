504ae1f5a8c282365fb1cd686231ce531d193ff9
Jack Cogdill <hi@jackcogdill.com>
2023-08-22 19:46:43 -0700

    vim: switch from lightline -> lualine

---
+++ nvim/lua/plugins/lualine.lua
@@ -0,0 +1,59 @@
+-- status line
+return {
+  'nvim-lualine/lualine.nvim',
+  lazy = false,
+  opts = {
+    options = {
+      icons_enabled = false,
+      component_separators = '|',
+      section_separators = '',
+      color = {
+        gui = '', -- no bold or italics
+      },
+    },
+    sections = {
+      lualine_a = {
+        {
+          'mode',
+          fmt = function(s)
+            return s:sub(1, 3) -- limit to 3 characters
+          end,
+        },
+      },
+      lualine_b = { 'filename' },
+      lualine_c = {},
+      lualine_x = { 'filetype' },
+      lualine_y = { 'progress' },
+      lualine_z = { 'location' },
+    },
+    inactive_sections = {
+      lualine_a = {},
+      lualine_b = {},
+      lualine_c = { 'filename' },
+      lualine_x = { 'location' },
+      lualine_y = {},
+      lualine_z = {},
+    },
+    tabline = {
+      lualine_a = {
+        {
+          'tabs',
+          max_length = function()
+            return vim.o.columns
+          end,
+          mode = 1, -- tab_name
+          tabs_color = {
+            active = 'lualine_b_normal',
+            inactive = 'lualine_c_normal',
+          },
+          component_separators = { left = '', right = '' },
+        },
+      },
+      lualine_b = {},
+      lualine_c = {},
+      lualine_x = {},
+      lualine_y = {},
+      lualine_z = {},
+    },
+  },
+}
