7a0a2bd3a7d65fa3daffcb2717b060f87efc625e
Jack Cogdill <hi@jackcogdill.com>
2023-08-23 14:40:45 -0700

    vim: disable whitespace plugin for terminals

--- nvim/lua/plugins/vim-better-whitespace.lua
+++ nvim/lua/plugins/vim-better-whitespace.lua
@@ -6,5 +6,9 @@ return {
     vim.keymap.set('n', '<C-s>', function()
       vim.cmd.StripWhitespace()
     end, { silent = true })
+    vim.api.nvim_create_autocmd(
+      'TermOpen',
+      { pattern = '*', command = 'DisableWhitespace' }
+    )
   end,
 }
