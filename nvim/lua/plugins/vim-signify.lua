f023c64f9d0076da9b4b549ac12434a626f1c2bd
Jack Cogdill <hi@jackcogdill.com>
2023-08-22 20:20:48 -0700

    vim: improve vim-signify config (lazier, use lua)

--- nvim/lua/plugins/vim-signify.lua
+++ nvim/lua/plugins/vim-signify.lua
@@ -1,12 +1,15 @@
 -- signs for changes tracked by a version control system
 return {
   'mhinz/vim-signify',
-  event = 'BufReadPost',
+  event = 'VeryLazy',
   config = function()
-    vim.cmd([[
-      let g:signify_skip = { 'vcs': { 'allow': ['git'] } }
-      let g:signify_update_on_focusgained = 1
-    ]])
+    vim.g.signify_skip = {
+      vcs = {
+        allow = { 'git' },
+      },
+    }
+    vim.g.signify_update_on_focusgained = true
     pcall(require, 'local.vim-signify')
+    vim.cmd.SignifyEnable()
   end,
 }
