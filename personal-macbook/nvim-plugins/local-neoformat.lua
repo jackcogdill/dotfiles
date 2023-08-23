d8483d3907980c1b887bdf07d24767e4de6bde33
Jack Cogdill <hi@jackcogdill.com>
2023-08-22 18:50:41 -0700

    vim: configure lua formatter

--- personal-macbook/nvim-plugins/local-neoformat.lua
+++ personal-macbook/nvim-plugins/local-neoformat.lua
@@ -5,17 +5,27 @@ return {
   config = function()
     vim.g.neoformat_python_yapf = {
       exe = 'yapf',
-      args = { "--style='{based_on_style: google, indent_width: 2}'" }
+      args = { "--style='{based_on_style: google, indent_width: 2}'" },
     }
     vim.g.neoformat_typescript_prettier = {
       exe = 'prettier',
-      args = { '--parser typescript --single-quote' }
+      args = { '--parser typescript', '--single-quote' },
     }
     vim.g.neoformat_javascript_prettier = {
       exe = 'prettier',
-      args = { '--parser javascript --single-quote' }
+      args = { '--parser javascript', '--single-quote' },
     }
-    vim.g.neoformat_enabled_lua = { 'stylua' }
+    vim.g.neoformat_lua_stylua = {
+      exe = 'stylua',
+      args = {
+        '--call-parentheses Always',
+        '--indent-type Spaces',
+        '--quote-style AutoPreferSingle',
+        '--indent-width 2',
+        '--column-width 80',
+      },
+      replace = true,
+    }
     vim.api.nvim_create_autocmd('BufWritePre', {
       pattern = '*',
       command = 'Neoformat',
