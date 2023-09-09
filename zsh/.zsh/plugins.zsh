c25bc3f82fe775023ca51cde89cbad9f0545f2e3
Jack Cogdill <hi@jackcogdill.com>
2023-09-09 10:18:40 -0700

    chore(zsh): code cleanup

--- zsh/.zsh/plugins.zsh
+++ zsh/.zsh/plugins.zsh
@@ -82,4 +82,4 @@ zstyle :bracketed-paste-magic paste-finish pastefinish
 # p10k
 # ----
 # To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
-[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
+[[ -f ~/.p10k.zsh ]] && source ~/.p10k.zsh
