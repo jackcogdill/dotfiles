709139d85687884502b4f40958e3110e178c8c6f
Jack Cogdill <hi@jackcogdill.com>
2023-09-08 22:28:23 -0700

    simplify local machine config structure

--- personal-macbook/install.sh
+++ personal-macbook/install.sh
@@ -3,6 +3,8 @@
 cd $(dirname "$0")
 
 # NeoVim
+pushd nvim >/dev/null
 mkdir -p ~/.config/nvim/lua/local
-stow --target ~/.config/nvim/lua/local nvim-local
-stow --target ~/.config/nvim/lua/plugins nvim-plugins
+stow --target ~/.config/nvim/lua/local local
+stow --target ~/.config/nvim/lua/plugins plugins
+popd >/dev/null
