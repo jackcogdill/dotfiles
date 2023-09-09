3cd3d38604858d039977127a07ec461cc8e0dff7
Jack Cogdill <hi@jackcogdill.com>
2023-09-09 10:17:28 -0700

    chore(zsh): code cleanup

--- zsh/.zsh/plugins.zsh
+++ zsh/.zsh/plugins.zsh
@@ -64,15 +64,18 @@ plugin-source $plugins
 # ---------------
 bindkey -M emacs '^[^M' autosuggest-execute # Alt + Enter
 bindkey -M vicmd '^[^M' autosuggest-execute # Alt + Enter
+
 # This speeds up pasting
 # https://github.com/zsh-users/zsh-autosuggestions/issues/238
 function pasteinit() {
   OLD_SELF_INSERT=${${(s.:.)widgets[self-insert]}[2,3]}
   zle -N self-insert url-quote-magic # I wonder if you'd need `.url-quote-magic`?
 }
+
 function pastefinish() {
   zle -N self-insert $OLD_SELF_INSERT
 }
+
 zstyle :bracketed-paste-magic paste-init pasteinit
 zstyle :bracketed-paste-magic paste-finish pastefinish
 
