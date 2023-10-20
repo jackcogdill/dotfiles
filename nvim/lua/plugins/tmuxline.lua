5eb6f0d2e3f6c2cef812541292a79ce0bd19cae3
Jack Cogdill <hi@jackcogdill.com>
2023-10-20 14:39:01 -0700

    chore: convert vimscript to lua

--- nvim/lua/plugins/tmuxline.lua
+++ nvim/lua/plugins/tmuxline.lua
@@ -6,20 +6,18 @@ return {
   cmd = 'Tmuxline',
   config = function()
     vim.g.tmuxline_powerline_separators = false
-    vim.cmd([[
-      let g:tmuxline_preset = {
-            \ 'a'       : '#h:#S',
-            \ 'win'     : '#W',
-            \ 'cwin'    : '#W',
-            \ 'x'       : '#T',
-            \ 'y'       : '%a %b %d',
-            \ 'z'       : '%H:%M',
-            \ 'options' : {
-            \   'status-justify' : 'left',
-            \   'status-style'   : 'default',
-            \ },
-            \ }
-    ]])
+    vim.g.tmuxline_preset = {
+      a = '#h:#S',
+      win = '#W',
+      cwin = '#W',
+      x = '#T',
+      y = '%a %b %d',
+      z = '%H:%M',
+      options = {
+        ['status-justify'] = 'left',
+        ['status-style'] = 'default',
+      },
+    }
     if ok then
       mod.config()
     end
