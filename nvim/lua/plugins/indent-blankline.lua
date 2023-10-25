bf8ee2414007bc0b0c7f94b02f04fbb0c673b759
Jack Cogdill <hi@jackcogdill.com>
2023-10-25 12:00:37 -0700

    vim: use latest version of indent-blankline

--- nvim/lua/plugins/indent-blankline.lua
+++ nvim/lua/plugins/indent-blankline.lua
@@ -1,15 +1,8 @@
 -- indentation guides
 return {
   'lukas-reineke/indent-blankline.nvim',
-  version = '2', -- everforest does not support v3 yet
+  version = '*',
   event = 'VeryLazy',
-  opts = {
-    enabled = true,
-    use_treesitter = true,
-    max_indent_increase = 1,
-    show_first_indent_level = false,
-    show_trailing_blankline_indent = false,
-    show_current_context = true,
-    show_current_context_start = true,
-  },
+  main = 'ibl',
+  opts = {},
 }
