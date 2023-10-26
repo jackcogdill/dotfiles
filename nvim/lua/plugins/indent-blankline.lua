2414b76c0b8c9e777371541538faeeebe77942c3
Jack Cogdill <hi@jackcogdill.com>
2023-10-26 11:23:41 -0700

    [vim] fix(ibl): improve scope start highlight

--- nvim/lua/plugins/indent-blankline.lua
+++ nvim/lua/plugins/indent-blankline.lua
@@ -3,14 +3,14 @@ return {
   'lukas-reineke/indent-blankline.nvim',
   version = '*',
   event = 'VeryLazy',
-  main = 'ibl',
   opts = {
     indent = {
       char = '│',
     },
-    scope = {
-      show_start = false,
-      show_end = false,
-    },
   },
+  config = function(_, opts)
+    require('ibl').setup(opts)
+    vim.cmd.highlight('clear @ibl.scope.underline.1')
+    vim.cmd.highlight('link @ibl.scope.underline.1 IndentBlanklineContextStart')
+  end,
 }
