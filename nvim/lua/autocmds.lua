732eb522d8f3adc2065da0f237fc86cbe31a73b5
Jack Cogdill <hi@jackcogdill.com>
2024-11-07 16:51:20 -0800

    [vim] feat(ui): rm autocmd to style terminals

--- nvim/lua/autocmds.lua
+++ nvim/lua/autocmds.lua
@@ -9,11 +9,3 @@ vim.api.nvim_create_autocmd(
   'FocusGained',
   { pattern = '*', command = 'checktime' }
 )
-
-vim.api.nvim_create_autocmd('TermOpen', {
-  pattern = '*',
-  callback = function()
-    vim.o.ft = 'terminal'
-    vim.o.number = false
-  end,
-})
