5d61f5da3b21e351c5a39014f62c5c495e09238e
Jack Cogdill <hi@jackcogdill.com>
2025-05-02 11:26:27 -0700

    [vim] refactor(everforest): remove unused option

--- nvim/lua/plugins/everforest.lua
+++ nvim/lua/plugins/everforest.lua
@@ -7,7 +7,6 @@ return {
     vim.g.everforest_background = 'soft'
     vim.g.everforest_enable_italic = 1
     vim.g.everforest_sign_column_background = 'none'
-    vim.g.everforest_lightline_disable_bold = 1
     vim.g.everforest_float_style = 'dim'
     vim.cmd.colorscheme('everforest')
   end,
