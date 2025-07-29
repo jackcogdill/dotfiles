7b0b6991964cd08f330e4c64c3a8ca8f0a710fdc
Jack Cogdill <hi@jackcogdill.com>
2025-07-28 17:58:40 -0700

    [vim] feat: disable unused keymap to dedup map prefix

--- nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/lua/plugins/nvim-lspconfig.lua
@@ -36,7 +36,7 @@ return {
       )
 
       local opts = { noremap = true, silent = true, buffer = bufnr }
-      vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
+      -- vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
       vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
       vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
       vim.keymap.set('n', 'g0', vim.lsp.buf.document_symbol, opts)
