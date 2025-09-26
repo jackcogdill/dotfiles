588a7dae050718ef8e6f4dda98a2aaa1e8ac8d28
Jack Cogdill <hi@jackcogdill.com>
2025-09-26 16:12:31 -0700

    [zsh] fix: colors for gemini cli

--- zsh/.zsh/exports.zsh
+++ zsh/.zsh/exports.zsh
@@ -1,6 +1,7 @@
 export EDITOR=$(which nvim)
 export VISUAL=$(which nvim)
 export PAGER="$(which less) -i" # i: smart case search
+export COLORTERM=truecolor
 
 # fzf
 export FZF_DEFAULT_OPTS="--color=16" # Match terminal color scheme
