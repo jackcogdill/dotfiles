66e8e73f6b4d3b1ca8382ea5853061ed63e3a3ad
Jack Cogdill <hi@jackcogdill.com>
2025-11-11 14:34:11 -0800

    [macos] feat: disable alternate character menu. use repeat

--- install/macos.sh
+++ install/macos.sh
@@ -1,4 +1,5 @@
 # Key repeat
+defaults write -g ApplePressAndHoldEnabled -bool false # disable alternate character menu
 defaults write -g InitialKeyRepeat -int 15 # normal minimum is 15 (225 ms)
 defaults write -g KeyRepeat -int 1 # normal minimum is 2 (30 ms)
 echo "Key repeat changes require restart"
