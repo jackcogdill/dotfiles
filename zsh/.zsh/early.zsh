28ee42da48af8ce31e595c917fb61e85b4fb99e2
Jack Cogdill <hi@jackcogdill.com>
2026-03-13 14:23:52 -0700

    [zsh] feat: auto tmux infinite alternate sessions

--- zsh/.zsh/early.zsh
+++ zsh/.zsh/early.zsh
@@ -1,16 +1,33 @@
 printf '\e[1 q' # Blinking block cursor
 bindkey -e # Emacs
 
-# Auto tmux
-if [[ $- == *i* && -z "$TMUX" ]]; then
-  [[ -n "$SSH_CONNECTION" ]] && local session="ssh" || local session="tmux"
-  if tmux ls | grep -q "(attached)"; then
-    # create new session in the same group
-    tmux new -A -s "${session}-alt" -t "${session}"
-  else
-    tmux new -A -s "${session}"
+function _auto_tmux() {
+  local session="tmux"
+  [[ -n $SSH_CONNECTION ]] && session="ssh"
+
+  # session does not exist
+  if ! 2>/dev/null tmux has -t "$session"; then
+    tmux new -t "$session"
+    return
   fi
-fi
+
+  # session exists and is not attached
+  if [[ -n $(tmux ls -f "#{&&:#{==:#{session_name},$session},#{==:#{session_attached},0}}") ]]; then
+    tmux attach -t "$session"
+    return
+  fi
+
+  # find or create an alternate session in the same group
+  local i=0
+  until [[ -z $(tmux ls -f "#{&&:#{==:#{session_name},$session$i},#{==:#{session_attached},1}}") ]]; do
+    (( i++ ))
+  done
+
+  tmux new -A -s "$session$i" -t "$session"
+}
+
+# Auto tmux
+[[ $- == *i* && -z "$TMUX" ]] && _auto_tmux
 
 # Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
 # Initialization code that may require console input (password prompts, [y/n]
